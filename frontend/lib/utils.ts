9a73d669fb44c8638a31774b55cb15d67ff08fe6
4Fort <dexterfortbsilva@gmail.com>
2024-10-05 00:17:44 +0800

    Added: Date formatter for chat bubbles

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,6 +1,36 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(dateString: string) {
+  const date = new Date(dateString);
+  const now = new Date();
+
+  const time = date.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  });
+  const isToday = date.toDateString() === now.toDateString();
+
+  const yesterday = new Date();
+  yesterday.setDate(now.getDate() - 1);
+  const isYesterday = date.toDateString() === yesterday.toDateString();
+
+  if (isToday) {
+    return `Today, ${time}`;
+  } else if (isYesterday) {
+    return `Yesterday, ${time}`;
+  } else if (date.getFullYear() === now.getFullYear()) {
+    return `${date.toLocaleString("en-US", {
+      month: "long",
+    })} ${date.getDate()}, ${time}`;
+  } else {
+    return `${date.toLocaleString("en-US", {
+      month: "long",
+    })} ${date.getDate()}, ${date.getFullYear()} ${time}`;
+  }
 }
